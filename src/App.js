8c69af0bc9d3762fd802d36b78c1fc64ed7df5a6
mousumiahmed <mousumiahmed786@gmail.com>
2024-03-27 22:20:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Checkbox from "./Checkbox";
+import { Catalogues } from "./mock";
 
-function App() {
+const App = () => {
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(Catalogues);
+  }, [list]);
+
+  const handleSelectAll = (e) => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(list.map((li) => li.id));
+    if (isCheckAll) {
+      setIsCheck([]);
+    }
+  };
+
+  const handleClick = (e) => {
+    const { id, checked } = e.target;
+    setIsCheck([...isCheck, id]);
+    if (!checked) {
+      setIsCheck(isCheck.filter((item) => item !== id));
+    }
+  };
+
+  console.log(isCheck);
+ 
+
+  const catalog = list.map(({ id, name }) => {
+    console.log(id,name);
+    return (
+      <>
+      <div>
+        <Checkbox
+          key={id}
+          type="checkbox"
+          name={name}
+          id={id}
+          handleClick={handleClick}
+          isChecked={isCheck.includes(id)}
+        />
+        {name}
+        </div>
+      </>
+      
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Checkbox
+        type="checkbox"
+        name="selectAll"
+        id="selectAll"
+        handleClick={handleSelectAll}
+        isChecked={isCheckAll}
+      />
+      Select All
+      <div>{catalog}</div>    
+    </>
   );
-}
+};
 
 export default App;
